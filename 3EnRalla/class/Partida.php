a03cb03a60d1ca542b174836d5a65542d86ffce6
Miguel Pérez <miguelperez@Beginners-Mac.local>
2025-03-03 09:18:45 +0100

    Lógica creada

--- 3EnRalla/class/Partida.php
+++ 3EnRalla/class/Partida.php
@@ -6,7 +6,7 @@ class Partida{
     protected $tablero;
 
 
-    public function __construct($ganador = "x", $turno = "0", $tablero = [["O"],["X"],["X"],[""],[""],[""],[""],[""],[""]]){
+    public function __construct($ganador = "", $turno = "O", $tablero = [[""],[""],[""],[""],[""],[""],[""],[""],[""]]){
         $this->ganador = $ganador;
         $this->turno = $turno;
         $this->tablero = $tablero;
@@ -18,15 +18,23 @@ class Partida{
         echo"<div class='container'>";
         $tablero = $this->tablero;
         for ($i=0; $i<9; $i++){
-                echo '<form class="casilla"><input type="hidden" name="borrar" value="' . $i . '">
-                <input type="submit" name="borrar" value=' . $tablero[$i][0] . '></input></form>';
+                echo '<form class="casilla" method="post"><input type="hidden" name="borrar" value="' . $i . '">
+                <input type="submit" name="mostrar" value=' . $tablero[$i][0] . '></input></form>';
             }
     echo "</div>";
     }
 
+    public function turno($manager){
+        $arrayObjetos = $manager->extract();
+        $turno = $arrayObjetos[0]->getTurno();
+        if ($turno == "O"){
+            $turno = "X";
+        }else {
+            $turno = "O";
+        }
+        $arrayObjetos[0]->setTurno($turno);
 
-    public function turno(){
-        
+        $manager->insert($arrayObjetos[0]);
     }
 
     public function toArray(){
@@ -37,34 +45,88 @@ class Partida{
         ];
     }
 
-
     public static function toObjeto($data){
         return new Partida($data["ganador"], $data["turno"], $data["tablero"]);
 
     }
 
-
-
-
-    /**
-     * Get the value of tablero
-     */ 
-    public function getTablero()
-    {
-        return $this->tablero;
+    public function jugada($play, $manager){
+        $libro = $manager->extract();
+        $turno = $libro[0]->getTurno();
+        $tablero = $libro[0]->getTablero();
+        $tablero[$play] = [$turno];
+        $libro[0]->setTablero($tablero);
+        $manager->insert($libro[0]);
     }
 
-    /**
-     * Set the value of tablero
-     *
-     * @return  self
-     */ 
-    public function setTablero($tablero)
-    {
-        $this->tablero = $tablero;
 
-        return $this;
-    }
+
+
+    /** Getters y setters
+         * Get the value of tablero
+         */ 
+        public function getTablero()
+        {
+            return $this->tablero;
+        }
+
+        /**
+         * Set the value of tablero
+         *
+         * @return  self
+         */ 
+        public function setTablero($tablero)
+        {
+            $this->tablero = $tablero;
+
+            return $this;
+        }
+
+        /**
+         * Get the value of turno
+         */ 
+        public function getTurno()
+        {
+            return $this->turno;
+        }
+
+        /**
+         * Set the value of turno
+         *
+         * @return  self
+         */ 
+        public function setTurno($turno)
+        {
+            $this->turno = $turno;
+
+            return $this;
+        }
+
+        /**
+         * Get the value of ganador
+         */ 
+        public function getGanador()
+        {
+            return $this->ganador;
+        }
+
+        /**
+         * Set the value of ganador
+         *
+         * @return  self
+         */ 
+        public function setGanador($ganador)
+        {
+            $this->ganador = $ganador;
+
+            return $this;
+        }
+
+    /* */
+
+
+
+
 }
     
     
--- 3EnRalla/index.php
+++ 3EnRalla/index.php
@@ -7,18 +7,31 @@
     <link rel="stylesheet" href="styles.css">
 </head>
 <body>
-
+    <form method="post">
+        <input type="submit" name="reset" value="Reset">
+    </form>
 </body>
 </html>
 
 <?php
 require_once("./class/Partida.php");
 require_once("./class/Manager.php");
+
+
 $Partida = new Partida();
-#$Partida->mostrartablero();
 $Manager = new Manager("partida.json");
-$Manager->insert($Partida);
+
+$Inicio = isset($_POST["borrar"]) ? True : False;
+$Inicio = isset($_POST["reset"]) ? False : True;
+if($Inicio == false){
+$Manager->insert($Partida);}
+
+
+$movimiento = isset($_POST["borrar"]) ? $_POST["borrar"] : "";
+$Partida->turno($Manager);
+$Partida->jugada($movimiento, $Manager);
 $Manager->read();
+
 
 
 
